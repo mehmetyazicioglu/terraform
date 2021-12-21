ab159b192e452109f281b81b452cc902ef03200b
mehmet <mehmetyazicioglu@gmail.com>
2021-12-21 21:50:07 +0000

    ec2 instasnce count

--- main.tf
+++ main.tf
@@ -32,6 +32,7 @@ resource "random_pet" "sg" {}
 resource "aws_instance" "web" {
   ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
+  count         	 = var.instance_count
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 variable "instance_name" {
   description = "Value of the name tag for the ec2 intance"
   type        = string
-  default     = "ExampleAppserverInstance"
+  default     = "ExampleAppserverInstance1"
+}
+
+variable "instance_count" {
+ description = "value of the instance count"
+ default     = "2"
 }
